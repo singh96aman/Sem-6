60589df984c554d06c4ef2e970daabaa3910eda8
singh96aman <singh96aman@gmail.com>
2018-01-17 10:55:48 +0530

    Ques3 Lab2 CD

--- Compiler Design/lab2/ques3.c
+++ Compiler Design/lab2/ques3.c
@@ -5,7 +5,7 @@ int main()
 {
 	FILE *fa, *fb;
 	char filename[50];
-	char keywords[5][50] = {"int","main","if","else","char"};
+	char keywords[6][50] = {"int","main","if","else","char","return"};
 	printf("\n Enter the filename to open: ");
 	scanf("%s",filename);
 	fa = fopen(filename,"r");
@@ -22,22 +22,25 @@ int main()
 	while(ca!=EOF){
 		if(ca==' ' || ca=='(' || ca=='\n' || ca=='\t'){
 			buffer[index]='\0';
-			putc(ca,fb);
-			for(i=0; i<5; i++)
+			for(i=0; i<6; i++)
 				if(strcmp(buffer,keywords[i])==0)
 					{
-						flag=0;
-						fseek(fa,-strlen(buffer), SEEK_CUR);
+						//fseek(fa,-1*index, SEEK_CUR);
 						for(k=0; buffer[k]!='\0'; k++)
-							putc(buffer[k]-32,fb);
+							buffer[k]-=32;
 					}
+			for(k=0; buffer[k]!='\0'; k++)
+				putc(buffer[k],fb);
+			putc(ca,fb);
 			index=0;
 		}else{
 			buffer[index++]=ca;
-			putc(ca,fb);
+			//putc(ca,fb);
 		}
 		ca = getc(fa);
 	}
+	for(k=0; buffer[k]!='\0'; k++)
+		putc(buffer[k],fb);
 	fclose(fa);
 	fclose(fb);
 	return 0;
